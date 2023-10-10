8f3fa1d2901d80263c15cd46b3af18e94383f17f
OmarHanchi <omarelhanchi@gmail.com>
2023-10-10 22:52:53 +0100

    playground first commit

--- flask/fundamentals/Routing/server.py
+++ flask/fundamentals/Routing/server.py
@@ -1,23 +1,26 @@
 from flask import Flask , render_template
 app= Flask(__name__)
-@app.errorhandler(404)
-def not_found():
-    return render_template("error.html")
+
+    
 @app.route ("/")
 def index () : 
-    return ("Hello World !")
+    return render_template("index.html")
 @app.route ("/Dojo")
 def dojo ():
     return ("Dojo!")
 @app.route ("/hi/<name>")
 def hi_name (name):
     return f"Hi {name.capitalize()} !"
+@app.route ('/hello/<string:name>/<int:num>')
+def hello_name (name,num):
+    return render_template ("hello.html",name=name,num=num)
 @app.route('/repeat/<int:num>/<string:word>')
 def repeat_word(num, word):
     output = ''
     for i in range(0,num):
         output += f"<p>{word}</p>"
     return output
+
 if __name__ == "__main__":
     app.run(debug=True)
 
